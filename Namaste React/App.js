32d386a4b1b76a70eab2ebd54ca9baee7c54f49d
Piyush Agarwal <143875622+piyushcoder12@users.noreply.github.com>
2024-05-08 10:50:14 +0530

    initial commit

---
+++ Namaste React/App.js
@@ -0,0 +1,32 @@
+// const heading = React.createElement(
+//     "h1",
+//     {id : "heading", xyz:"ansddd"},
+//     "Hello world from React");
+
+//     console.log(heading); // object
+
+// const root = ReactDOM.createRoot
+// (document.getElementById("root"));
+
+// root.render(heading);
+
+const parent = React.createElement(
+    "div"
+    ,{id:"parent"},[
+    React.createElement("div",{id:"child1"},[
+        React.createElement("h1",{},"I 'm an h1 tag"),
+        React.createElement("h2",{},"I am an h2 tag"),
+
+    ]),
+    React.createElement("div",{id:"child2"},[
+        React.createElement("h1",{},"I 'm an h1 tag"),
+        React.createElement("h2",{},"I am an h2 tag"),
+    ]),
+]);
+console.log(parent); // object
+
+const root = ReactDOM.createRoot (document.getElementById("root"));
+
+root.render(parent);
+
+// jsx 
